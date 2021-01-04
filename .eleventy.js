ae4711493c98f724776fbf275a153c10f1e82597
Travis Ricks <travis@travisricks.com>
2021-01-04 14:00:26 +0900

    add markdown support for furigana

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,25 @@
 module.exports = function (eleventyConfig) {
+  let markdownIt = require('markdown-it')
+  let options = {
+    html: true,
+    typographer: true,
+  }
+
+  let markdownItFurigana = require('furigana-markdown-it')({
+    fallbackParens: '()',
+    extraSeparators: '-',
+    extraCombinators: "'",
+  })
+  let markdownLib = markdownIt(options).use(markdownItFurigana)
+
+  eleventyConfig.setLibrary('md', markdownLib)
+
   return {
     dir: {
-      input: "src",
-      output: "dist",
+      input: 'src',
+      output: 'dist',
     },
-    templateFormats: ["html", "md", "njk"],
+    templateFormats: ['html', 'md', 'njk'],
     passthroughFileCopy: true,
-  };
-};
+  }
+}
