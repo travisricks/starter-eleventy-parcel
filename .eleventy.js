77d31ae98cd4214f49f8935447079761d6d7e0d8
Travis Ricks <travis@travisricks.com>
2021-01-04 14:28:54 +0900

    add 404 page

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 module.exports = function (eleventyConfig) {
   let markdownIt = require('markdown-it')
   let markdownItOptions = {
@@ -13,6 +15,22 @@ module.exports = function (eleventyConfig) {
   let markdownLib = markdownIt(markdownItOptions).use(markdownItFurigana)
 
   eleventyConfig.setLibrary('md', markdownLib)
+
+  // configure Browsersync to handle 404 routing when using --serve
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function (err, bs) {
+        bs.addMiddleware('*', (req, res) => {
+          const content_404 = fs.readFileSync('dist/404.html')
+          // Add 404 http status code in request header.
+          res.writeHead(404, { 'Content-Type': 'text/html; charset=UTF-8' })
+          // Provides the 404 content without redirect.
+          res.write(content_404)
+          res.end()
+        })
+      },
+    },
+  })
 
   return {
     dir: {
